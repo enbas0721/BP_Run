b29a3ed60b864076087b6b9d21b594df72561aed
enbas0721 <maruyama.kento.98@gmail.com>
2026-01-25 22:09:36 +0900

    障害物生成システムを作成（変更予定）

--- Assets/Scripts/Level/SegmentBase.cs
+++ Assets/Scripts/Level/SegmentBase.cs
@@ -2,26 +2,42 @@ using UnityEngine;
 
 public class SegmentBase : MonoBehaviour
 {
-    public Transform endPoint;
-    public Transform obstaclePoints;
+    [SerializeField] private Transform endPoint;
+
+    [Header("Obstacle")]
+    [SerializeField] private Transform obstaclePoints;
+    [SerializeField] private Transform obstacleInstancesRoot;
+
+    [Header("Item")]
+    [SerializeField] private Transform itemPoints;
+    [SerializeField] private Transform itemInstancesRoot;
 
     public float SegmentLength => endPoint.localPosition.z;
 
     public void RebuildObstacles(ObstaclePlacer placer)
     {
-        if (!obstaclePoints || !placer) return;
+        if (!obstaclePoints || !placer  || !obstacleInstancesRoot) return;
 
-        for (int i = obstaclePoints.childCount - 1; i >= 0; i--)
+        for (int i = obstacleInstancesRoot.childCount - 1; i >= 0; i--)
         {
-            var child = obstaclePoints.GetChild(i);
-
-            if (child.GetComponent<ObstacleSpawnPoint>() != null) continue;
-
-            Destroy(child.gameObject);
+            Destroy(obstacleInstancesRoot.GetChild(i).gameObject);
         }
 
+        /* [TODO] 他のSpawnPointでの配置状況を考慮していないため全レーンに配置されうる*/
         /* 確率によって、セグメント内に障害物を配置するかどうかはPlacerが決める */
-        placer.Place(obstaclePoints);
+        placer.Place(obstaclePoints, obstacleInstancesRoot);
+    }
+
+    public void RebuildItems(ItemPlacer placer)
+    {
+        if (!placer || !itemPoints || !itemInstancesRoot) return;
+
+        for (int i = itemInstancesRoot.childCount - 1; i >= 0; i--)
+        {
+            Destroy(itemInstancesRoot.GetChild(i).gameObject);
+        }
+
+        placer.Place(itemPoints, itemInstancesRoot);
     }
 
     public float GetEndZ()
--- Assets/Scripts/Level/SegmentSpawner.cs
+++ Assets/Scripts/Level/SegmentSpawner.cs
@@ -7,16 +7,21 @@ using UnityEngine;
 
 public class SegmentSpawner : MonoBehaviour
 {
-    public Transform player;
-    public SegmentPool pool;
-    public ObstaclePlacer obstaclePlacer;
+    [SerializeField] private Transform player;
+    [SerializeField] private SegmentPool pool;
+
+    [Header("Obstacle")]
+    [SerializeField] private ObstaclePlacer obstaclePlacer;
+
+    [Header("Item")]
+    [SerializeField] private ItemPlacer itemPlacer;
 
     [Header("Spawn Control")]
     [Tooltip("プレイヤーの前方に確保したい床の距離")]
-    public float aheadDistance = 60f;
+    [SerializeField] private float aheadDistance = 60f;
 
     [Tooltip("初期に敷く最低枚数（見た目のため）")]
-    public int initialSegments = 4;
+    [SerializeField] private int initialSegments = 4;
 
     private float spawnZ = 0f;
 
@@ -42,6 +47,7 @@ public class SegmentSpawner : MonoBehaviour
 
         /* セグメント内への障害物の破壊と生成はセグメントに任せる。 */
         seg.RebuildObstacles(obstaclePlacer);
+        seg.RebuildItems(itemPlacer);
 
         spawnZ += seg.SegmentLength;
     }
